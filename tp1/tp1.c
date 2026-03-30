99d004671b7549faf189659ceb1e6cd17ede7665
lasnelus <malabretienne@gmail.com>
2026-03-30 08:49:56 +0200

    \n

--- tp1/tp1.c
+++ tp1/tp1.c
@@ -8,7 +8,7 @@ void main(void)
 
 void affiche_Grille(void)
 {
-    printf("  0 1 2 3 4 5 6 7 8 9%n");
+    printf("  0 1 2 3 4 5 6 7 8 9\n");
     for(int i=0; i<10;i++)
     {
         printf("%d",i);
