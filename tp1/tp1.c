88d4983e25cb52048a04592440be62d774cd2ea3
lasnelus <malabretienne@gmail.com>
2026-03-30 08:49:10 +0200

    first thing first

--- tp1/tp1.c
+++ tp1/tp1.c
@@ -3,5 +3,19 @@
 
 void main(void)
 {
-    printf("pizza");
+    affiche_Grille();
+}
+
+void affiche_Grille(void)
+{
+    printf("  0 1 2 3 4 5 6 7 8 9%n")
+    for(int i=0; i<10;i++)
+    {
+        printf("%d",i);
+        for(int j=0;j<10;j++)
+        {
+            // printf("%s", grid[i][j]);
+            printf(".");
+        }
+    }
 }
