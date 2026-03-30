0767610723dc1b346b11fdc45868cd902ca13ca1
lasnelus <malabretienne@gmail.com>
2026-03-30 09:13:14 +0200

    fire at will

--- tp1/tp1.h
+++ tp1/tp1.h
@@ -1,8 +1,12 @@
 #define NB_COLONNE 10
 #define NB_LIGNE 10
 
-typedef char *Grille[NB_LIGNE][NB_COLONNE];
+typedef char Grille[NB_LIGNE][NB_COLONNE];
 
 void affiche_Grille(Grille grid);
 
 void add_boat(Grille *grid, int latitude, int longitude);
+
+void tirer (Grille *grid, int latitude, int longitude);
+
+bool check_Touche (Grille grid, int latitude, int longitude);
