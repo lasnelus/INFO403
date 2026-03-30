cf492be5011d55fbabe6f9242ca246ef35a95e50
lasnelus <malabretienne@gmail.com>
2026-03-30 11:14:25 +0200

    oopsie

--- tp1/tp1.h
+++ tp1/tp1.h
@@ -21,15 +21,15 @@ void affiche_grille(TypeGrille grille);
 
 void add_bateau(TypeGrille *grille, int latitude, int longitude);
 
-void tirer (Joueur *joueur, int latitude, int longitude, int tour);
+void tirer (Joueur *joueur, int latitude, int longitude, int *tour);
 
 bool check_touche (TypeGrille grille, int latitude, int longitude);
 
 void generer_bateaux_aleatoire(TypeGrille *grille, int nb_bateaux);
 
-void tire_aleatoire(Joueur *joueur, int tour);
+void tire_aleatoire(Joueur *joueur, int *tour);
 
 
-void tirer_UI(Joueur *joueur, int tour);
+void tirer_UI(Joueur *joueur, int *tour);
 
 void placage_bateaux_UI(TypeGrille *grille, int nb_bateaux);
