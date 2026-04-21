9b79569e5fbfa5ce26c46645e239f8cb0462ed83
lasnelus <malabretienne@gmail.com>
2026-04-21 10:12:31 +0200

    h instead of H

--- td5/hash.c
+++ td5/hash.c
@@ -3,7 +3,7 @@ void main (void)
 
 }
 
-void init_hashtable(int size, Hashtable *table)
+void init_hashtable(int size, hashtable *table)
 {
     table->size = size;
     table->person = malloc(size * sizeof(Person *));
