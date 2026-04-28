6fe3686860f7981273d82db68c646e679b7a7d41
lasnelus <malabretienne@gmail.com>
2026-04-28 10:04:56 +0200

    type

--- tp3-4/annuaire.c
+++ tp3-4/annuaire.c
@@ -136,7 +136,7 @@ void affiche_menu(Annuaire *annuaire)
                 break;
 
             case 2:
-                lister_contacte_annuaire(annuaire);
+                lister_contacte_annuaire(*annuaire);
                 break;
 
             case 3:
